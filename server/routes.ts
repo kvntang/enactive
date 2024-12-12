b4e219476a1dc9d7bb5164463190ea8f8e34c01f
Kevin Tang <63424931+kvntang@users.noreply.github.com>
2024-12-12 00:58:12 -0500

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -172,7 +172,7 @@ class Routes {
         messages: [
           {
             role: "user",
-            content: `Strictly generate a list of 36 phrases similar to this scene: ${prompt}. Rank them from most similar to most dissimilar. The list will be string phrases seperated by only comma, that is it, no labeling or indexing of any kind. For example: "a white cat, a black dog, a jump cat"`,
+            content: `Strictly generate a list of 36 word phrases similar to this scene: ${prompt}. Order them from most similar to most dissimilar. The list will be phrases seperated by only comma, that is it, no labeling or indexing of any kind. For example: "a white cat, a black dog, a jump cat"`,
           },
         ],
       }),
